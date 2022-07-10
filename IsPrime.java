4f45bbe97781b50b4d7207f80fe99e8924ca4b33
ArockiaSybilPerez <109013417+ArockiaSybilPerez@users.noreply.github.com>
2022-07-09 22:17:00 -0700

    Add files via upload

---
+++ IsPrime.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class IsPrime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a = 11;
+       int b = 1;
+       
+       for (int i = 2; i>-1; i++) {
+    	    b = a%i;
+       }
+       
+       if (true) 
+    	   
+    	   System.out.println("prime number");
+       
+       else
+    	   
+    	   System.out.println("Not prime number");
+       }
+	
+}
